ff645abf7e516da07aad661aa46ba21c22ea6647
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-04-07 08:57:04 -0700

    Precursor to final figures document

---
+++ R/4.1_figures_tables.R
@@ -0,0 +1,19 @@
+# Publication Figures and Tables
+# Need to ouput appropriate results from individual files
+
+
+source("R/0_utilities.R")
+
+theme_set(theme_bw(base_size = 14))
+
+# ---------------------------------------------------------------------
+# Import data
+
+
+
+# ---------------------------------------------------------------------
+# Figure 1
+
+
+# ---------------------------------------------------------------------
+# Figure 2
