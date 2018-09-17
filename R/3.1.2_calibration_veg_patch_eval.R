a6b37ce07e30b41d06c1dfcb2f10516e35ac343e
ryanrbart <ryanrbart@eri.ucsb.edu>
2018-09-17 14:29:20 -0700

    Code to calibrate vegetation parameters

---
+++ R/3.1.2_calibration_veg_patch_eval.R
@@ -0,0 +1,30 @@
+# Contains scripts for evaluating vegetation patch calibration
+# P301 and B204
+
+
+source("R/0_utilities.R")
+
+# ---------------------------------------------------------------------
+# Import and process
+
+happy_p301 <- readin_rhessys_output("ws_p301/out/3.1_p301_cal_veg_patch/p301_cal",c=1,p=1,g=1)
+happy_b204 <- readin_rhessys_output("ws_b204/out/3.1_b204_cal_veg_patch/b204_cal",c=1, p=1, g=1)
+
+# Combine canopy data
+cd <- as_tibble(dplyr::bind_rows("p301" = happy_p301$cd, "b204" = happy_b204$cd, .id = "watershed"))
+cd$stratumID <- factor(cd$stratumID, levels=c(1,2))
+
+# ---------------------------------------------------------------------
+# Plot figures
+
+# Compare height
+x <- ggplot(data = cd) +
+  geom_line(aes(x=wy, y=height, linetype=stratumID, color=watershed))
+plot(x)
+
+
+# Compare LAI
+x <- ggplot(data = cd) +
+  geom_line(aes(x=wy, y=lai, linetype=stratumID, color=watershed))
+plot(x)
+
